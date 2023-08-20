2ce1156a24b03600b7fe64fdd92ace6f0fd6869e
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-08-20 18:39:42 -0300

    add data jpa repository

--- src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Timestamp;

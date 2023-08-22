834fc30c714036370ee77d96c047f65f753d3ee2
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-08-22 17:10:16 -0300

    fix broken tests

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-08-22T17:00:01-0300",
+    date = "2023-08-22T17:08:00-0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.20 (Ubuntu)"
 )
 @Component

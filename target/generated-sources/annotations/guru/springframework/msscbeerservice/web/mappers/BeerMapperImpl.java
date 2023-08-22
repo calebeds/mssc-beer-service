c0e08615f62529f0ad4240ba911b507212d49118
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-08-22 17:32:28 -0300

    add lombok and mapstruct conf

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-08-22T17:08:00-0300",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.20 (Ubuntu)"
+    date = "2023-08-22T17:31:45-0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.20 (Ubuntu)"
 )
 @Component
 public class BeerMapperImpl implements BeerMapper {

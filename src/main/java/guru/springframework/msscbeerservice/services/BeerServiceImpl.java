a2a88a739d94b597a9e9f792320d49394a49cbd9
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-08-30 17:30:32 -0300

    implement controller methods

---
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
@@ -0,0 +1,42 @@
+package guru.springframework.msscbeerservice.services;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import guru.springframework.msscbeerservice.web.controller.NotFoundException;
+import guru.springframework.msscbeerservice.web.mappers.BeerMapper;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class BeerServiceImpl implements BeerService {
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public BeerDto getBeerBy(UUID beerId) {
+        return beerMapper.beerToBeerDto(
+                beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
+        );
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return beerMapper.beerToBeerDto(
+                beerRepository.save(beerMapper.beerDtoToBeer(beerDto))
+        );
+    }
+
+    @Override
+    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+        //going to look for the beer, before updating it
+        beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+
+        return beerMapper.beerToBeerDto(
+                beerRepository.save(beerMapper.beerDtoToBeer(beerDto))
+        );
+    }
+}

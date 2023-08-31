94667c33e7cc5cba9e87c49bd8a2aea5235c33cc
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-08-31 18:59:30 -0300

    add list of beers and enhance with inventory information

--- src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -3,6 +3,7 @@ package guru.springframework.msscbeerservice.domain;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 import org.hibernate.annotations.UpdateTimestamp;
 
 
@@ -21,17 +22,22 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+   @Type(type = "org.hibernate.type.UUIDCharType")
     @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
+
     @Version
     private Long version;
+
     @CreationTimestamp
     @Column(updatable = false)
     private Timestamp createdDate;
+
     @UpdateTimestamp
     private Timestamp lastModifiedDate;
     private String beerName;
     private String beerStyle;
+
     @Column(unique = true)
     private String upc;
     private BigDecimal price;
--- src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
@@ -1,6 +1,9 @@
 package guru.springframework.msscbeerservice.services;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerPageList;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 
 import java.util.UUID;
@@ -11,4 +14,6 @@ public interface BeerService {
     BeerDto saveNewBeer(BeerDto beerDto);
 
     BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+
+    BeerPageList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest of);
 }

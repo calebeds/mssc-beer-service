15afad79aa0bc2f7def30bb798496eeff055d465
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-09-14 11:11:28 -0300

    add jms config

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-09T15:43:21-0300",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
+    date = "2023-09-13T11:55:29-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
 )
 @Component
 @Qualifier("delegate")

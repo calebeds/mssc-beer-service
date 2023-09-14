c56822fd3e76a63d9b16d4582c0662699d560501
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-09-14 11:07:39 -0300

    configure h2 mysql compatibility mode

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-07T17:42:18-0300",
+    date = "2023-09-08T18:29:26-0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
 )
 @Component

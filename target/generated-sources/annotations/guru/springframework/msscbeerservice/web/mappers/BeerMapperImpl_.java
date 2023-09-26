2dd53b69f0eda96684d508ade73db7f77f42aeb4
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-09-26 17:06:01 -0300

    update parent bom. Closes #1

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-17T13:53:00-0300",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
+    date = "2023-09-26T17:05:26-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
 )
 @Component
 @Qualifier("delegate")

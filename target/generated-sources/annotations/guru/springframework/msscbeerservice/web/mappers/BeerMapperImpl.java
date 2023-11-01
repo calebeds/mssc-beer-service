529e53b7c82f9b6920297805830d35e14a09c7ed
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-10-31 21:15:03 -0300

    Add and Configure Spring Cloud Sleuth - close #7

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-29T17:33:43-0300",
+    date = "2023-10-30T20:08:08-0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
 )
 @Component

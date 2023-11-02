d4e1b233c7161dad419083bdcb900ea6617ca6ad
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-11-02 15:28:23 -0300

    update bom to 1.0.13

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-31T21:35:33-0300",
+    date = "2023-11-02T15:25:19-0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
 )
 @Component

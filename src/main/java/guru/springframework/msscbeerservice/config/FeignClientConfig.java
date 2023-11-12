f6332b612481de5f79475e78c50b45e0f3b73773
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-11-12 15:08:48 -0300

    Configure HTTP Basic for Feign Client - close #11

---
+++ src/main/java/guru/springframework/msscbeerservice/config/FeignClientConfig.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+
+@Configuration
+public class FeignClientConfig {
+    @Bean
+    public BasicAuthenticationInterceptor basicAuthenticationInterceptor(@Value("${sfg.brewery.inventory-user}") String inventoryUser,
+                                                                            @Value("${sfg.brewery.inventory-password}") String inventoryPassword) {
+        return new BasicAuthenticationInterceptor(inventoryUser, inventoryPassword);
+    }
+}
